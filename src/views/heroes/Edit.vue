5ecc7a1fc18c92f7ebc711ea80415c4146bd1f66
goddlts <54337467@qq.com>
2018-09-02 16:27:31 +0800

    编辑英雄-显示英雄详细信息

--- src/views/heroes/Edit.vue
+++ src/views/heroes/Edit.vue
@@ -1,12 +1,49 @@
 <template>
   <div>
-    edit {{ id }}
+    <h2 class="sub-header">修改英雄</h2>
+    <form>
+      <div class="form-group">
+        <label for="txtName">英雄名称</label>
+        <input v-model="formData.name" type="text" class="form-control" id="txtName" placeholder="英雄名称">
+      </div>
+      <div class="form-group">
+        <label for="txtgender">英雄性别</label>
+        <input v-model="formData.gender" type="text" class="form-control" id="txtgender" placeholder="性别">
+      </div>
+      <button type="submit" class="btn btn-success">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      formData: {
+        name: '',
+        gender: ''
+      }
+    }
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    // 根据id，获取英雄的详细信息
+    loadData() {
+      axios
+        .get(`http://127.0.0.1:3001/heroes/${this.id}`)
+        .then((response) => {
+          this.formData = response.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
 };
 </script>
 
