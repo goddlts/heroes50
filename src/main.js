163b327387ec4afe57e4c29ddeef865f09d2c539
goddlts <54337467@qq.com>
2018-09-02 14:54:57 +0800

    提取路由模块

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@
 
 // ES6模块化的语法
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue';
 
 // 导入全局的样式
@@ -12,26 +11,8 @@ import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.css';
 
-// 导入(路由)组件
-import HeroList from './views/heroes/List.vue';
-import WeaponList from './views/weapons/List.vue';
-import EquipList from './views/equips/List.vue';
-
-
-// Vue.use()注册VueRouter插件
-Vue.use(VueRouter);
-
-// 创建路由对象
-const router = new VueRouter({
-  // 设置精确的激活类样式
-  linkExactActiveClass: 'active',
-  // 路由规则
-  routes: [
-    { path: '/heroes', component: HeroList },
-    { path: '/equips', component: EquipList },
-    { path: '/weapons', component: WeaponList }
-  ]
-});
+// 导入路由模块
+import router from './routers';
 
 new Vue({
   el: '#app',
---
+++ src/routers/index.js
@@ -0,0 +1,26 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+
+// 导入(路由)组件
+import HeroList from '../views/heroes/List.vue';
+import WeaponList from '../views/weapons/List.vue';
+import EquipList from '../views/equips/List.vue';
+
+// Vue.use()注册VueRouter插件
+Vue.use(VueRouter);
+
+// 创建路由对象
+const router = new VueRouter({
+  // 设置精确的激活类样式
+  linkExactActiveClass: 'active',
+  // 路由规则
+  routes: [
+    { path: '/', redirect: '/heroes' },
+    { path: '/heroes', component: HeroList },
+    { path: '/equips', component: EquipList },
+    { path: '/weapons', component: WeaponList }
+  ]
+});
+
+// 导出对象
+export default router;
