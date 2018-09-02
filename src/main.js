43465071185155cd65bf915ee8fbd863e51cd7d5
goddlts <54337467@qq.com>
2018-09-02 10:57:16 +0800

    配置首页

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+// 基于webpack的开发，是基于模块化的开发
+// node.js的模块化遵守CommonJS规范
+// var Vue = require('vue');
+
+// ES6模块化的语法
+import Vue from 'vue';
+import App from './App.vue';
+
+// 导入全局的样式
+// 导入bootstrap的样式  3.3.7
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css';
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,13 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.(ttf|woff2|woff|eot)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
       }
     ]
   },
